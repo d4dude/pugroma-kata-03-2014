355fbb8d5eee31a9e40eb26a97e9c929bd75b6cd
d4dude <Simo@MacBook-Pro-di-Simo.local>
2014-11-04 14:57:37 +0100

    Terzo commit di d4dude

--- src/PUGRoma/Kata/Kata.php
+++ src/PUGRoma/Kata/Kata.php
@@ -10,14 +10,14 @@ class Kata
 
         for ($i = 1; $i < 101; $i++)
         {
-            if ($i%3 = 0){
-                if ($i%5 = 0) {
+            if ($i%3 == 0){
+                if ($i%5 == 0) {
                     $coda[$i]= "FizzBuzz";
                 }
                 $coda[$i]= "Fizz";
             }
 
-            if ($i%5 = 0) {
+            if ($i%5 == 0) {
                 $coda[$i]= "Buzz";
             }
             else $coda[$i]= $i;
