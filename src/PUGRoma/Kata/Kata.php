c8109c37f0fb112c4592a395f995f7ba7347646d
d4dude <Simo@MacBook-Pro-di-Simo.local>
2014-11-04 14:50:51 +0100

    Primo commit di d4dude

--- src/PUGRoma/Kata/Kata.php
+++ src/PUGRoma/Kata/Kata.php
@@ -1,14 +1,29 @@
 <?php
 
-namespace PUGRoma\Kata;
+// namespace PUGRoma\Kata;
 
 class Kata
 {
     public function isReady()
     {
-        return true;
+    public $coda[];
+
+        for ($i = 1; $i < 101; $i++)
+        {
+            if ($i%3 = 0){
+                if ($i%5 = 0) {
+                    $coda[$i]= "FizzBuzz";
+                }
+                $coda[$i]= "Fizz";
+            }
+
+            if ($i%5 = 0) {
+                $coda[$i]= "Buzz";
+            }
+            else $coda[$i]= $i;
+        }
+        return $coda;
+
     }
 }
-
-
 
