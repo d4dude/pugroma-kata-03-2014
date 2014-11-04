e6b5b25dc4f91969c88e132d15946ec98d541351
Claudio D'Alicandro <claudio.dalicandro@gmail.com>
2014-11-04 18:32:48 +0100

    Proposta
    Questa è una pull request :D

--- src/PUGRoma/Tests/KataTest.php
+++ src/PUGRoma/Tests/KataTest.php
@@ -9,22 +9,29 @@ class KataTest extends \PHPUnit_Framework_TestCase
         $kata = new Kata();
         //$this->assertTrue($kata->isReady());
 
-        $client = $kata->BuzFizzer();
+        // Però secondo il kata questa funzione dovrebbe tornare una stringa, non un array, o mi sbaglio?!?
+        $results = $kata->BuzFizzer();
 
-        foreach ($client as $client)
+        foreach ($results as $result)
         {
-            $crawler = array_search('Buzz', $client);
-            $crawler = array_search('Fizz', $client);
-            $crawler = array_search('BuzzFizz', $client);
-
-            if( isset ($crawler))
+            // Devi utilizzare variabili diverse senno sovrascrivi
+            $buzzPosition     = array_search('Buzz', $result);
+            $fizzPosition     = array_search('Fizz', $result);
+            $buzzfizzPosition = array_search('BuzzFizz', $result);
+            
+            // Così testi solo i primi fizz, buzz e fizzbuzz, perché array_search torna la prima posizione
+            // che trova... Questo va bene, non devi testare tutto il risultato, bastano i casi particolari
+            // però devi esserne consapevole
+            
+            foreach (array($buzzPosition, $fizzPosition, $buzfizzPosition) as $position)
             {
-             if ($crawler%3 != 0 || $crawler%5 != 0)
-             {
-                 echo "Sparo l'eccezzione";
-             }
+                // Ecco come si utilizzano le asserzioni
+                $this->assertTrue($position % 3 == 0 || $position % 5 == 0 || $position % 15 == 0);
+                
+                // Il test cmq non è corretto allo stato attuale, perché la logica testa solo che 
+                // alle posizioni 3, 5, e 15 ci sia fizz buzz o fizzbuzz, ma passa pure se alla
+                // posizione 3 c'è buzz  
             }
-
         }
     }
 }
