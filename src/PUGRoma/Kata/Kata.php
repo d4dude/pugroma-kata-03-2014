7cc31e5f575342c189e5f5529b400bb54db49303
d4dude <Simo@MacBook-Pro-di-Simo.local>
2014-11-04 15:32:26 +0100

    Quarto commit di d4dude

--- src/PUGRoma/Kata/Kata.php
+++ src/PUGRoma/Kata/Kata.php
@@ -1,29 +1,35 @@
 <?php
 
-namespace PUGRoma\Kata;
+//namespace PUGRoma\Kata;
 
 class Kata
 {
+    $contenitore = array();
+
     public function isReady()
     {
-    public $coda[];
-
-        for ($i = 1; $i < 101; $i++)
+    for ($i = 1; $i<=100; $i++)
         {
-            if ($i%3 == 0){
-                if ($i%5 == 0) {
-                    $coda[$i]= "FizzBuzz";
+        if ($i%3 == 0)
+            {
+             if ($i%5 == 0)
+                {
+                 $contenitore[$i]="FizzBuzz"."\n";
                 }
-                $coda[$i]= "Fizz";
             }
 
-            if ($i%5 == 0) {
-                $coda[$i]= "Buzz";
-            }
-            else $coda[$i]= $i;
-        }
-        return $coda;
+        else{$contenitore[$i]="Fizz"."\n";}
 
+         if ($i%5 == 0)
+         {$contenitore[$i]="Buzz"."\n";}
+
+         else{$contenitore[$i]=$i."\n";}
+
+        }//CHIUDE FOR
+
+     return $contenitore;
     }
+
 }
 
+$smazzamelo = new Kata();
